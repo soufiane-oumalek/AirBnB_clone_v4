93b220d0a8f59cafbb96fc9f5efa7db735fdf17b
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-11-04 20:46:57 +0100

    updates

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -7,7 +7,6 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -19,7 +18,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb', strict_slashes=False)
+@app.route('/2-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
